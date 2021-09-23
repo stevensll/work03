c35da986c8a0604123911c18fd4cdd1e74fb6178
Steven <deltaorb637@gmail.com>
2021-09-22 20:19:11 -0400

    completed question 5, but quite slow

--- euler.c
+++ euler.c
@@ -2,7 +2,9 @@
 
 int main(){
     int three_five(int threshold);
+    int smallest_multiple(int number);
     printf("Sum of first 1000 multiples of three or five: %d\n", three_five(1000));
+    printf("Smallest multiple of every number from 1-20: %d\n", smallest_multiple(20));
     return 0;
 }
 
@@ -17,4 +19,29 @@ int three_five(int threshold){
         threshold--;
     }
     return sum;
+}
+
+int smallest_multiple(int number){
+    int counter = 1;
+    int found = 1;
+    int status = 0;
+
+    while(status == 0){
+        found = 1;
+        int i = 1;
+        for(int i = 1; i < number; i++){
+            if(counter % i != 0){
+                found = 0;
+            }
+        }
+        if(found ==1){
+            found = counter;
+            status =1;
+        } else{
+            counter++;
+        }
+
+    }
+    return found;
+
 }
